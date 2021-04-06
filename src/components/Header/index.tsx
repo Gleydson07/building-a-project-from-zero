790a562be55e386d40e1c2151cd3213dc152756d
Gleydson Albuquerque <gassantos.dev@gmail.com>
2021-04-05 23:34:42 -0300

    start commit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,13 @@
-export default function Header() {
-  // TODO
+import style from './header.module.scss'
+
+export function Header() {
+  return(
+    <header className={style.headerContainer}>
+      <nav className={style.headerContent}>
+        <a href="/">
+          <img src="/images/logo.svg" alt="logo"/>
+        </a>
+      </nav>
+    </header>
+  )
 }
